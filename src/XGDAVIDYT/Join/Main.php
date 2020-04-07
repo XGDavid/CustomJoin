436fdd594022a1d3580a53d9bb43b2d3b42ed8e6
XGDavid <39276408+XGDavid@users.noreply.github.com>
2020-04-07 20:15:54 +0300

    Update Main.php

--- src/XGDAVIDYT/Join/Main.php
+++ src/XGDAVIDYT/Join/Main.php
@@ -16,8 +16,6 @@ use pocketmine\Server;
 use pocketmine\entity\Effect;
 use pocketmine\entity\EffectInstance;
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\CommandSender;
-use pocketmine\command\Command;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\utils\Config;
@@ -29,7 +27,6 @@ class Main extends PluginBase implements Listener{
     public function onEnable() : void{
         $this->saveResource("config.yml");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-
         $this->getLogger()->info("§6§l§kiii§r§6[TCG]§a JoinEffectAndMessage has enabled. §6§l§kiii§r§6");
         $this->getLogger()->info("§6§l§kiii§r§6[TCG]§a Thx for use plugin. Plugin by XGDAVIDYT §6§l§kiii§r§6");
         $this->getLogger()->info($this->getConfig()->get("PluginEnable"));
@@ -42,8 +39,10 @@ class Main extends PluginBase implements Listener{
 
         $player = $event->getPlayer();
         $name = $player->getName();
-        //$joineffect = $this->getConfig()->get("JoinEffect")
-        $effect = new EffectInstance(Effect::getEffect(10), 20 * 15, 1); //10 = effect - ticks * time - level
+        $joineffect = $this->getConfig()->get("JoinEffect");
+        $duration = $this->getConfig()->get("Duration");
+        $level = $this->getConfig()->get("Level");
+        $effect = new EffectInstance(Effect::getEffect($joineffect), 20 * $duration, $level); //10 = effect - ticks * time - level
         
         $effect->setVisible(false);
         $player->addEffect($effect);
