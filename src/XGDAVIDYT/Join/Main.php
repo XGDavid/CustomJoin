55514024da6e349fa3de8ddbd494b3028c82e565
XGDavid <39276408+XGDavid@users.noreply.github.com>
2020-04-13 21:00:43 +0300

    Update Join Message 1 & 2
    add:
    JoinMessage1 & JoinMessage2 - in config

--- src/XGDAVIDYT/Join/Main.php
+++ src/XGDAVIDYT/Join/Main.php
@@ -1,6 +1,9 @@
 <?php
 
-/* All rights reserved XGTeam ©2020
+/*
+ * Please do not remove ANY Code/Modify or Copy!
+ * Standard Copyright License applied.
+ * All rights reserved XGTeam ©2020
  * www.youtube.com/xgdavid
  */
 
@@ -24,6 +27,8 @@ class Main extends PluginBase implements Listener{
     public function onEnable() : void{
         $this->saveResource("config.yml");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("§6§l§kiii§r§6[TCG]§a JoinEffectAndMessage has enabled. §6§l§kiii§r§6");
+        $this->getLogger()->info("§6§l§kiii§r§6[TCG]§a Thx for use plugin. Plugin by XGDAVIDYT §6§l§kiii§r§6");
         $this->getLogger()->info($this->getConfig()->get("PluginEnable"));
     } 
 
@@ -35,13 +40,15 @@ class Main extends PluginBase implements Listener{
         $player = $event->getPlayer();
         $name = $player->getName();
         $joineffect = $this->getConfig()->get("JoinEffect");
+        $joinmessage1 = $this->getConfig()->get("JoinMessage1");
+        $joinmessage2 = $this->getConfig()->get("JoinMessage2");
         $duration = $this->getConfig()->get("Duration");
         $level = $this->getConfig()->get("Level");
         $effect = new EffectInstance(Effect::getEffect($joineffect), 20 * $duration, $level); //10 = effect - ticks * time - level
         
         $effect->setVisible(false);
         $player->addEffect($effect);
-        $event->setJoinMessage($name . $this->getConfig()->get("JoinMessage"));
+        $event->setJoinMessage($joinmessage1 . $name . $joinmessage2);
         
 
     return true;
