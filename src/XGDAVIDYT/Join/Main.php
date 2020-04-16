7a1b1973fc9c9e7d068ea18166d49aaf55b73ac0
XGDavid <39276408+XGDavid@users.noreply.github.com>
2020-04-16 20:12:59 +0300

    Update Main.php

--- src/XGDAVIDYT/Join/Main.php
+++ src/XGDAVIDYT/Join/Main.php
@@ -36,15 +36,14 @@ class Main extends PluginBase implements Listener{
         $player = $event->getPlayer();
         $name = $player->getName();
         $joineffect = $this->getConfig()->get("JoinEffect");
-        $joinmessage1 = $this->getConfig()->get("JoinMessage1");
-        $joinmessage2 = $this->getConfig()->get("JoinMessage2");
+        $joinmessage1 = $this->getConfig()->get("JoinMessage");
         $duration = $this->getConfig()->get("Duration");
         $level = $this->getConfig()->get("Level");
         $effect = new EffectInstance(Effect::getEffect($joineffect), 20 * $duration, $level); //10 = effect - ticks * time - level
         
         $effect->setVisible(false);
         $player->addEffect($effect);
-        $event->setJoinMessage($joinmessage1 . $name . $joinmessage2);
+        $event->setJoinMessage($joinmessage);
         
 
     return true;
